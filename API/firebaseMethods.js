f78b6c54a73946a87a744b3a665f4335ac3ec82c
dogukan_901 <doki9706@gmail.com>
2021-12-21 05:45:43 +0300

    firebase mobile app connections initialization and authentication settings initialized

---
+++ API/firebaseMethods.js
@@ -0,0 +1,41 @@
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { doc, setDoc, getFirestore } from "firebase/firestore";
+import { Alert } from "react-native";
+
+export async function registration(email, password, lastName, firstName) {
+    try {
+        const auth = getAuth();
+        await createUserWithEmailAndPassword(auth, email, password);
+        const currentUser = auth.currentUser;
+
+        const db = getFirestore();
+
+        // Add a new document in collection "users"
+        await setDoc(doc(db, "users", currentUser.uid), {
+            email: currentUser.email,
+            lastName: lastName,
+            firstName: firstName
+        });
+
+    } catch (err) {
+        Alert.alert("There is something wrong!!!!", err.message);
+    }
+}
+
+export async function signIn(email, password) {
+    try {
+        const auth = getAuth();
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+        Alert.alert("There is something wrong!", err.message);
+    }
+}
+
+export async function loggingOut() {
+    try {
+        const auth = getAuth();
+        await signOut(auth);
+    } catch (err) {
+        Alert.alert('There is something wrong!', err.message);
+    }
+}
